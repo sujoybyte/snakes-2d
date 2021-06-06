40d5229709d0d9812e879fadb95d546ad6d2c0a2
Sujoy Ghosh <sujoybyte@gmail.com>
2021-06-06 06:16:31 +0530

    added: snake movement with arrow keys

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
 #include "raylib.h"
 
-const int width = 800, height = 800;
-Vector2 center = {(float)width/2, (float)height/2};
+const int screenWidth = 800, screenHeight = 800;
+Vector2 center = {(float) screenWidth / 2, (float) screenHeight / 2};
 Vector2 snake[200] = {}, tempSnake[200] = {};
 int snakeLength = 0;
 
+void LoadSnake();
+void MoveSnake(int stepX, int stepY);
 
-void LoadSnake()
-{
-    // initialize snake head
-    snake[0] = center;
-    tempSnake[0] = center;
-    snakeLength++;
-    snake[1] = {snake[snakeLength-1].x-50, snake[snakeLength-1].y};
-    tempSnake[1] = {snake[snakeLength-1].x-50, snake[snakeLength-1].y};
-    snakeLength++;
-}
 
 int main()
 {
-    InitWindow(width, height, "snake game - raylib");
+    InitWindow(screenWidth, screenHeight, "snake game - raylib");
     SetTargetFPS(60);
 
     LoadSnake();
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_RIGHT)) MoveSnake(50, 0);
+        else if (IsKeyPressed(KEY_LEFT)) MoveSnake(-50, 0);
+        else if (IsKeyPressed(KEY_UP)) MoveSnake(0, -50);
+        else if (IsKeyPressed(KEY_DOWN)) MoveSnake(0, 50);
+
         BeginDrawing();
             ClearBackground(BLACK);
             for (int i = 0; i < snakeLength; i++)
@@ -39,4 +36,29 @@ int main()
     }
 
     CloseWindow();
+}
+
+void LoadSnake()
+{
+    // initialize snake head
+    snake[0] = center;
+    tempSnake[0] = center;
+    snakeLength++;
+    snake[1] = {snake[snakeLength-1].x-50, snake[snakeLength-1].y};
+    tempSnake[1] = {snake[snakeLength-1].x-50, snake[snakeLength-1].y};
+    snakeLength++;
+}
+
+void MoveSnake(int stepX, int stepY)
+{
+    // move snake 50 units
+    tempSnake[0] = snake[0];
+    snake[0].x = snake[0].x + stepX;
+    snake[0].y = snake[0].y + stepY;
+
+    for (int j = 1; j < snakeLength; j++)
+    {
+        tempSnake[j] = snake[j];
+        snake[j] = tempSnake[j-1];
+    }
 }
